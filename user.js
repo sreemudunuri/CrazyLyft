863cb70810675d9771e490d24c4bedd3d7b1b68b
jarretbryan <jarret.bryan@gmail.com>
2018-07-31 17:54:42 -0400

    edit css

--- user.js
+++ user.js
@@ -3,7 +3,7 @@ let usernameBox = document.getElementById('username-box')
 
 function renderUserName(userObj){
                   
-    let userHTML =  `<div class="card bg-dark text-white">
+    let userHTML =  `<div class="card bg-danger text-white">
                         <div class="card-body" id = '${userObj.id}'>
                             <h2 class="card-title">${userObj.name}</h2>
                             <p><strong> ID: ${userObj.id} </strong> </p>
