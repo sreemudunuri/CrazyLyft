2433436e1bdd1e22f6f42cc21c66ff667833f222
jarretbryan <jarret.bryan@gmail.com>
2018-08-01 10:40:55 -0400

    timer added with 45 seconds. indicates when time's up at 0

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-
-
+    
     let userForm = document.getElementById('username-form')
 
     userForm.addEventListener('submit', function(event){
         event.preventDefault()
         console.log(event.currentTarget[0].value)
-
-        
-
+       
         userAdapter.createUser(event.currentTarget[0].value)
         .then(res => {renderUserName(res)})
         .then(setInterval(run, 10))
+        .then(manageTheClock)
 
     })
 
---
+++ timer.js
@@ -0,0 +1,33 @@
+let timerHTML = document.getElementById('timer')
+
+let timer = parseInt(document.getElementById('timer').innerText)
+
+const countDown = () => {
+    // console.log(timer)
+    timerHTML.innerText = timer--
+}
+
+const add5sec = () => {
+    timerHTML.innerText = (timer +=5) 
+}
+
+const timerCheck = (countdownFn) => {
+    if (timer === 0) {
+        clearInterval(countdownFn)
+        
+        timerHTML.innerHTML = `<h3>Time's Up!</h3>`
+    }
+}
+
+const manageTheClock = () => {
+
+    let startTheClock = setInterval(countDown, 1000)
+
+    setInterval( () => {
+        timerCheck(startTheClock)
+    }, 1000  )
+    
+
+}
+
+// let startTheClock = setInterval(countDown, 1000)
